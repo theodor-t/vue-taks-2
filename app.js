29ec7bba470b7b6881e94aab2216352007b7bbb3
Theodor <54949643+theodorerlax@users.noreply.github.com>
2021-04-15 11:05:22 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,28 +2,34 @@ var app = new Vue({
     el: '#app',
     data: {
         username: "Tudor",
-        age: "0",
-        exp: "0"
-
+        age: 0,
+        exp: 0,
+        lang: 1,
+        expYears: 0,
+        salary: 0,
+        pos: 1,
+        coeff: [10000, 12000, 13500, 14000, 15000],
     },
 
     computed: {
-        calcAge: function () {
+        calcAge: function() {
             let currentDate = new Date();
             let birthDate = new Date(this.age);
             let difference = currentDate - birthDate;
             return Math.floor(difference / 31557600000);
 
-        },
-
-        calcExp: function () {
-            let currentDate = new Date();
-            let startDate = new Date(this.exp);
-            let difference = currentDate - startDate;
-            return Math.floor(difference / 31557600000);
-
         }
 
+    },
+    watch: {
+        lang: function(val) {
+            this.lang = parseFloat(val);
+            this.salary = this.coeff[this.pos - 1] * this.lang;
 
+        },
+        expYears: function(val) {
+            (val >= "5") ? this.pos = 5: this.pos = val;
+            this.salary = this.coeff[this.pos - 1] * this.lang;
+            }
     }
 })
